91a0eeb50e0d8b698a2633dc33ff64915ab7738c
mohammadiuser111-web <mohammadiuser111-web>
2026-08-02 22:00:07 +0000

    Serve avatar media and save profile image on selection

--- config/settings.py
+++ config/settings.py
@@ -15,5 +15,7 @@ DATABASES={'default':{'ENGINE':'django.db.backends.sqlite3','NAME':BASE_DIR/'db.
 AUTH_PASSWORD_VALIDATORS=[]
 LANGUAGE_CODE='fa-ir'; TIME_ZONE='Asia/Tehran'; USE_I18N=True; USE_TZ=True
 STATIC_URL='static/'; STATICFILES_DIRS=[BASE_DIR/'static']
+MEDIA_URL='/media/'
+MEDIA_ROOT=BASE_DIR/'media'
 DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
 LOGIN_URL='login'; LOGIN_REDIRECT_URL='dashboard'; LOGOUT_REDIRECT_URL='login'
