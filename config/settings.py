17c4246a9c1b1e62a95c0cac05fff9a138546981
mohammadiuser111-web <mohammadiuser111-web>
2026-08-05 12:40:06 +0000

    Configure STATIC_ROOT for production collectstatic

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,9 @@ WSGI_APPLICATION='config.wsgi.application'
 DATABASES={'default':{'ENGINE':'django.db.backends.sqlite3','NAME':BASE_DIR/'db.sqlite3'}}
 AUTH_PASSWORD_VALIDATORS=[]
 LANGUAGE_CODE='fa-ir'; TIME_ZONE='Asia/Tehran'; USE_I18N=True; USE_TZ=True
-STATIC_URL='static/'; STATICFILES_DIRS=[BASE_DIR/'static']
+STATIC_URL='static/'
+STATICFILES_DIRS=[BASE_DIR/'static']
+STATIC_ROOT=BASE_DIR/'staticfiles'
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR/'media'
 DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
